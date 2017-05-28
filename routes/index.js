5724922ce95bbb6e02e648856a0d7c593e0f2e69
Rick Walker <ricwalker59@gmail.com>
2017-05-28 12:27:35 -0400

    Added contact view

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,19 @@ const express = require('express');
 const cfc = require('../modules/cfc');
 const router = express();
 
-// get home page
+// get home view
 router.get('/', (req, res) => {
 	res.render('index', {title:cfc('home'), header:cfc('Home')});
 })
 
-// get add article
+// get about view
 router.get('/about', (req, res) => {
 	res.render('about', {title:cfc('about'), header:cfc('about us')});
+})
+
+// get contact view
+router.get('/contact', (req, res) => {
+	res.render('contact', {title:cfc('about'), header:cfc('contact us')});
 })
 
 module.exports = router;
