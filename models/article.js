a056435f35586309191505969dd22b438941494b
Rick Walker <ricwalker59@gmail.com>
2017-05-27 20:50:40 -0400

    Added url and date posted fields to the model

--- models/article.js
+++ models/article.js
@@ -13,6 +13,14 @@ let articleSchema = mongoose.Schema({
 	body: {
 		type: String,
 		required: true
+	},
+	postDate: {
+		type: String,
+		required: true,
+	},
+	url: {
+		type:String,
+		required: false
 	}
 });
 
--- routes/writeDb/index.js
+++ routes/writeDb/index.js
@@ -3,18 +3,18 @@ const mongoose = require('mongoose');
 const cfc = require('../../modules/cfc');
 const router = express();
 
-mongoose.connect('mongodb://_writer:writedeeznuts@localhost/nodekb');
-let db = mongoose.connection;
+// mongoose.connect('mongodb://_writer:writedeeznuts@localhost/nodekb');
+// let db = mongoose.connection;
 
 // check connection
-db.once('open', () => {
-	console.log('Connected to MongoDB');
-});
+// db.once('open', () => {
+// 	console.log('Connected to MongoDB');
+// });
 
 // check db errors
-db.on('error', (err) => {
-	console.log(err);
-});
+// db.on('error', (err) => {
+// 	console.log(err);
+// });
 
 let Article = require('../../models/article');
 
@@ -25,10 +25,21 @@ router.get('/articles/add', (req, res) => {
 
 // post add article
 router.post('/articles/add', (req, res) => {
+	mongoose.connect('mongodb://_writer:writedeeznuts@localhost/nodekb');
+	let date = new Date(),
+		year = date.getUTCFullYear(),
+		month = date.getUTCMonth() + 1,
+		day = date.getDate(),
+		hour = date.getUTCHours() + 1,
+		minute = date.getUTCMinutes() + 1,
+		seconds = date.getUTCSeconds() + 1,
+		timestamp = month.toString() + '/' + day.toString() + '/' + year;
 	let article = new Article();
 	article.title = req.body.title;
 	article.author = req.body.author;
 	article.body = req.body.body;
+	article.url = req.body.url;
+	article.postDate = timestamp;
 
 	article.save((err) => {
 		if(err) {
